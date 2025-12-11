50d447a072cef2193a77c387769e6df66622568e
Sok Koemsak <kimsak8899@gmail.com>
2025-12-11 21:12:59 +0700

    last

---
+++ script.js
@@ -0,0 +1,8 @@
+// alert("Hello would")
+console.log("testing")
+let x = 20
+let y = 30 
+// alert(x+y)
+document.writeln(x + y)
+var name = 'Kimsak khmer love'
+console.log("This is my name", name)
